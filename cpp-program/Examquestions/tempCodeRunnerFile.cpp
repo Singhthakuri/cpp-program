e140178baa9ecd438cdff052dde5735c52a608bd
Thakuri <gaju76960@gmail.com>
2025-05-21 16:36:09 +0545

    past-year-questions

---
+++ cpp-program/Examquestions/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// using namespace std;
+// inline void finCube(int l)
+// {
+//     cout << "cube of the box is:" << l * l * l << endl;
+// }
+// int main()
+// {
+//     int l;
+//     cout << "Enter the length of the box: ";
+//     cin >> l;
+//     finCube(l);
+//     return 0;
+// }
